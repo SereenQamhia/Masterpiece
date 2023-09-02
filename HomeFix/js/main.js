61918c60b032b7ab352e5e64bf7f75df07223862
SereenQamhia <Sereen.qamhia@gmail.com>
2023-09-02 23:10:27 +0300

    Done

---
+++ HomeFix/js/main.js
@@ -0,0 +1,216 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
+})(jQuery);
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+ 
+const day = document.querySelector(".calendar-dates");
+ 
+const currdate = document
+    .querySelector(".calendar-current-date");
+ 
+const prenexIcons = document
+    .querySelectorAll(".calendar-navigation span");
+ 
+// Array of month names
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+ 
+// Function to generate the calendar
+const manipulate = () => {
+ 
+    // Get the first day of the month
+    let dayone = new Date(year, month, 1).getDay();
+ 
+    // Get the last date of the month
+    let lastdate = new Date(year, month + 1, 0).getDate();
+ 
+    // Get the day of the last date of the month
+    let dayend = new Date(year, month, lastdate).getDay();
+ 
+    // Get the last date of the previous month
+    let monthlastdate = new Date(year, month, 0).getDate();
+ 
+    // Variable to store the generated calendar HTML
+    let lit = "";
+ 
+    // Loop to add the last dates of the previous month
+    for (let i = dayone; i > 0; i--) {
+        lit +=
+            `<li class="inactive">${monthlastdate - i + 1}</li>`;
+    }
+ 
+    // Loop to add the dates of the current month
+    for (let i = 1; i <= lastdate; i++) {
+ 
+        // Check if the current date is today
+        let isToday = i === date.getDate()
+            && month === new Date().getMonth()
+            && year === new Date().getFullYear()
+            ? "active"
+            : "";
+        lit += `<li class="${isToday}">${i}</li>`;
+    }
+ 
+    // Loop to add the first dates of the next month
+    for (let i = dayend; i < 6; i++) {
+        lit += `<li class="inactive">${i - dayend + 1}</li>`
+    }
+ 
+    // Update the text of the current date element
+    // with the formatted current month and year
+    currdate.innerText = `${months[month]} ${year}`;
+ 
+    // update the HTML of the dates element
+    // with the generated calendar
+    day.innerHTML = lit;
+}
+ 
+manipulate();
+ 
+// Attach a click event listener to each icon
+prenexIcons.forEach(icon => {
+ 
+    // When an icon is clicked
+    icon.addEventListener("click", () => {
+ 
+        // Check if the icon is "calendar-prev"
+        // or "calendar-next"
+        month = icon.id === "calendar-prev" ? month - 1 : month + 1;
+ 
+        // Check if the month is out of range
+        if (month < 0 || month > 11) {
+ 
+            // Set the date to the first day of the
+            // month with the new year
+            date = new Date(year, month, new Date().getDate());
+ 
+            // Set the year to the new year
+            year = date.getFullYear();
+ 
+            // Set the month to the new month
+            month = date.getMonth();
+        }
+ 
+        else {
+ 
+            // Set the date to the current date
+            date = new Date();
+        }
+ 
+        // Call the manipulate function to
+        // update the calendar display
+        manipulate();
+    });
+});
